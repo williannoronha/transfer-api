5904eda2afe3777716c75b259280a3c023d83817
Willian Noronha <willian.noronha@outlook.com>
2024-08-16 14:45:34 -0300

    Realizado refactory nas classes DTOs transformand-as no tipo record

--- src/main/java/com/itau/transferapi/model/dto/ClienteDTO.java
+++ src/main/java/com/itau/transferapi/model/dto/ClienteDTO.java
@@ -2,7 +2,6 @@ package com.itau.transferapi.model.dto;
 
 import java.math.BigDecimal;
 
-import jakarta.validation.constraints.NotNull;
 
 public record ClienteDTO (String nome, String numeroConta, BigDecimal saldo) {
 
